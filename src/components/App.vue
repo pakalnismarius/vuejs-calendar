90e3d0c39e1b60888d68268e63a47c5b27fbc2be
marius <pakalnis.marius@gmail.com>
2020-02-23 08:20:12 +0200

    Adding current-month componet

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div id="header">
+      <div>
+        <h1>Vue.js Calendar</h1>
+      </div>
+      <div>
+        <current-month />
+      </div>
+    </div>
     <div id="day-bar">
       <div>Mon</div>
       <div>Tue</div>
@@ -19,6 +27,7 @@
 
 <script type="text/javascript">
   import CalendarDay from './CalendarDay.vue';
+  import CurrentMonth from './CurrentMonth.vue';
 
   export default {
     computed: {
@@ -70,7 +79,8 @@
       }
     },
     components: {
-      CalendarDay
+      CalendarDay,
+      CurrentMonth
     }
   }
 </script>
---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>{{ formattedDate  }}</div>
+    <button @click="inc">+</button>
+    <button @click="dec">-</button>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    computed: {
+      year() {
+        return this.$store.state.currentYear;
+      },
+      month() {
+        return this.$store.state.currentMonth;
+      },
+      formattedDate() {
+        return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+      }
+    },
+    methods: {
+      inc() {
+
+      },
+      dec() {
+
+      }
+    }
+  }
+</script>
