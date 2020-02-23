cb241b3a9a2bd27762242d8c0d6ce137271aaefa
marius <pakalnis.marius@gmail.com>
2020-02-23 07:32:07 +0200

    Adding ui effects

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="day">{{ day.format('D') }}</div>
+  <div :class="classObject">{{ day.format('D') }}</div>
 </template>
 
 <script type="text/javascript">
   export default {
-    props: [ "day" ]
+    props: [ "day" ],
+    computed: {
+      classObject() {
+        let today =  this.day.isSame(this.$moment(), 'day');
+        return {
+          day: true,
+          today,
+          past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
+        };
+      }
+    }
   }
 </script>
